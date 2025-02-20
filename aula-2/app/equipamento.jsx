7cedc243c447e3d3f9924df916b30034abce3438
unknown <daniellsilva@outlook.com>
2025-02-19 22:01:28 -0300

    Aula 3

--- aula-2/app/equipamento.jsx
+++ aula-2/app/equipamento.jsx
@@ -1,38 +1,34 @@
 'use client'
 
 import { useState } from "react";
-import { Button, RangeSlider } from "flowbite-react"
+import { Alert, Button } from "flowbite-react"
 
-export default function Equipamento({title, defaultVolume}) {
-    const [volume, setVolume] = useState(defaultVolume);
-    const [disabled, setDisabled] = useState(false);
+export default function Equipamento({title, min, max, isOn}) {
+    const [volume, setVolume] = useState(min);
+    const [disabled, setDisabled] = useState(isOn);
 
     function handleIncreaseVolume() {
-        if (volume >= 10 || disabled) return;
+        if (volume == max || disabled) return;
 
         setVolume(volume + 1);
     }
-    
+
     function handleDecreaseVolume() {
-        if (volume <= 0 || disabled) return;
+        if (volume < min || disabled) return;
 
         setVolume(volume - 1);
     }
-
-    function handleVolumeChange(event) {
-        setVolume(Number(event.target.value));
-    }
-
     return (
         <div className={`flex flex-col space-x-2 space-y-4 items-center border-2 rounded-lg px-2 py-8 shadow w-96 dark:border-zinc-500 dark:shadow-zinc-800`}>
             <h1 className="font-bold text-2xl">{title}</h1>
             <div className="flex gap-4 items-center">
-                <Button color="light" onClick={handleDecreaseVolume}>-1</Button>
-                <Button color={(disabled) ? 'failure' : 'light'} onClick={() => {setVolume(0); setDisabled(!disabled)}}>Mute</Button>
-                <Button color="light" onClick={handleIncreaseVolume}>+1</Button>
+                <Button color={(!isOn || disabled) ? 'light' : 'blue'} onClick={handleDecreaseVolume}>-1</Button>
+                <Button color={(!isOn || disabled) ? 'light' : 'blue'} onClick={() => {setDisabled(!disabled);}}>{disabled ? 'Desligar' : 'Ligar'}</Button>
+                <Button color={(!isOn || disabled) ? 'light' : 'blue'} onClick={handleIncreaseVolume}>+1</Button>
             </div>
-            <RangeSlider className="w-56" type="range" onChange={handleVolumeChange} min={0} max={10} value={volume} disabled={disabled} />
-            <span className="text-gray-400 font-medium text-lg">{volume}</span>
+
+            <span className="text-gray-400 font-medium text-lg">{(volume < min) ? <Alert color="failure">O volume está abaixo do mínimo.</Alert> : volume}</span>
+            {(volume == max && !disabled) && <span className="text-emerald-500">Volume máximo</span>}
         </div>
     )
 }
--- aula-2/app/page.jsx
+++ aula-2/app/page.jsx
@@ -1,20 +1,30 @@
-import { DarkThemeToggle, Flowbite } from "flowbite-react";
+'use client'
+
+import { Button, DarkThemeToggle, Flowbite } from "flowbite-react";
 import Equipamento from "./equipamento";
+import { useState } from "react";
 
 export default function Home() {
+	const [isOn, setIsOn] = useState(true);
+
+	function handleTurnTableOn(){
+		setIsOn(!isOn);
+	}
+
 	return (
 		<Flowbite>
 			<main className="dark:bg-zinc-900 dark:text-zinc-300">
 				<div className="text-end">
 					<DarkThemeToggle className="border-2 m-5" />
 				</div>
-				<section className="flex flex-col items-center justify-center h-screen">
+				<section className="flex flex-col gap-4 items-center justify-center h-screen">
 					<h1 className="font-bold text-4xl">Mesa de som</h1>
+						<Button onClick={handleTurnTableOn}>{isOn ? "Desligar" : "Ligar"} mesa</Button>
 					<div className="grid grid-cols-3 m-8 gap-8">
-						<Equipamento title={'Microfone'} defaultVolume={3} color={'blue'} />
-						<Equipamento title={'Guitarra'} defaultVolume={4} />
-						<Equipamento title={'Bateria'} defaultVolume={6} />
-						<Equipamento title={'Baixo'} defaultVolume={6} />
+						<Equipamento title={'Microfone'} min={3} max={10} isOn={isOn} />
+						<Equipamento title={'Guitarra'} min={4} max={11} isOn={isOn} />
+						<Equipamento title={'Bateria'} min={6} max={12} isOn={isOn} />
+						<Equipamento title={'Baixo'} min={6} max={13} isOn={isOn} />
 					</div>
 				</section>
 			</main>
