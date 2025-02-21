d98bd779d8b06f89f70ef6c431ce5ce9a5122db5
unknown <daniellsilva@outlook.com>
2025-02-20 21:04:01 -0300

    Correção: vencedor da votação biblioteca

--- aula-4/app/biblioteca.jsx
+++ aula-4/app/biblioteca.jsx
@@ -5,10 +5,10 @@ import { Badge, Card } from "flowbite-react";
 export default function Biblioteca({ text, votes, winner }) {
     return (
         <Card className="w-44 h-36 text-center font-medium">
-            <span className="flex justify-center">
-                {winner && <Badge className="w-fit" size="xs" color="success">Vencedor</Badge>}
-            </span>
             {text} {winner && `- ${votes}`}
+            <span className="flex justify-center">
+                {winner == String(text).toLowerCase() && <Badge className="w-fit" size="xs" color="success">Vencedor</Badge>}
+            </span>
         </Card>
     )
 }
